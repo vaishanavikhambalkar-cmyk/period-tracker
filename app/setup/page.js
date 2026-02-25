5d2ea485f8d9d44ed9cc5e3c7f19028887da2170
vaishanavikhambalkar-cmyk <harshitkhambalkar@gmail.com>
2026-02-26 00:15:39 +0530

    Force dynamic setup page

--- app/setup/page.js
+++ app/setup/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import { useRouter, useSearchParams } from "next/navigation";
